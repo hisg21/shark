0c8fb4a3db2ab37083827a7b0f83f8c79ca52c01
Seunggoo Lee <hisg21@hotmail.com>
2014-08-05 16:59:32 +0900

    flush cached query to local file

--- src/main/scala/shark/SharkDriver.scala
+++ src/main/scala/shark/SharkDriver.scala
@@ -331,6 +331,7 @@ private[shark] class SharkDriver(conf: HiveConf) extends Driver(conf) with LogHe
         sem = cacheManager.get(hc).getSem
         plan = cacheManager.get(hc).getQp
         plan.getFetchTask.resetTotalRows()
+        cacheManager.flush(hc)
       } else {
         context = new QueryContext(conf, useTableRddSink)
         context.setCmd(command)
